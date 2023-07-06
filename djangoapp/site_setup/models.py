a9ceb5996bf809a3c72cfe3a3ecab9a018a4c316
Davi Costa <davicostadelira04@gmail.com>
2023-07-06 15:07:41 -0300

    Redimensionamento favicons

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.images import resize_image
+from utils.model_validators import validate_png
 
 # Create your models here.
 
@@ -36,8 +38,22 @@ class SiteSetup(models.Model):
 
     favicon = models.ImageField(
         upload_to='assets/favicon/%Y/%m/',
-        blank=True, default=''
+        blank=True, default='',
+        validators=[validate_png],
     )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)        
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed =  current_favicon_name != self.favicon.name
+
+        print('favicon_changed', favicon_changed)
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
