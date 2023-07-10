7357dffb8e4536fc0ea6223a1817932ec66b4390
Davi Costa <davicostadelira04@gmail.com>
2023-07-10 11:02:59 -0300

    Criando e Configurando Post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,7 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 from utils.rands import slugify_new
-
-# Create your models here.
 
 
 class Tag(models.Model):
@@ -17,10 +16,10 @@ class Tag(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify_new(self.name, 5)
+            self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
-    
-    def __str__(self):
+
+    def __str__(self) -> str:
         return self.name
 
 
@@ -37,15 +36,15 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify_new(self.name, 5)
+            self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
 class Page(models.Model):
-    title = models.CharField(max_length=65)
+    title = models.CharField(max_length=65,)
     slug = models.SlugField(
         unique=True, default="",
         null=False, blank=True, max_length=255
@@ -53,16 +52,71 @@ class Page(models.Model):
     is_published = models.BooleanField(
         default=False,
         help_text=(
-            'Este campo deve ser marcado caso queira que '
-            'a página seja exibida publicamente.'
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
         ),
-        )
+    )
     content = models.TextField()
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify_new(self.title, 5)
+            self.slug = slugify_new(self.title, 4)
         return super().save(*args, **kwargs)
-    
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para o post ser exibido publicamente.'
+        ),
+    )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Se marcado, exibirá a capa dentro do post.',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    # user.post_created_by.all
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_created_by'
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    # user.post_updated_by.all
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
