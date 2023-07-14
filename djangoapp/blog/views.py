c2aca9f4cecc90e63345eeb48efc0409f61bac48
Davi Costa <davicostadelira04@gmail.com>
2023-07-14 15:09:34 -0300

    Adicionando campo de pesquisa

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,5 +1,6 @@
 from blog.models import Post
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 
 # Create your views here.
@@ -70,6 +71,25 @@ def tag(request, slug):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+        }
+    )
+
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+    posts = Post.objects.get_published()\
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[0:PER_PAGE]
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
         }
     )
 
